6dd65c5b7a7bd87c8dac19093110357bc8f43887
nurill <mnurilhuda2005@gmail.com>
2026-02-26 13:42:06 +0700

    Jobsheet 2 praktikum 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,34 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+    return ('Hello World');
+});
+
+Route::get('/world', function () {
+ return 'World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+ return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+ return 'NIM: 244107020004 <br> Nama: Muhammad Nuril Huda';
+});
+
+Route::get('/user/{name}', function ($name) {
+ return 'Muhammad Nuril Huda '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId){
+ return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+ return 'Muhammad Nuril Huda '.$name;
 });
