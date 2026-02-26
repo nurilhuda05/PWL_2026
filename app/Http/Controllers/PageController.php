a19069b87ea4b3dee960f1191dccb53024bcc2c2
nurill <mnurilhuda2005@gmail.com>
2026-02-26 14:52:06 +0700

    Jobsheet 2 praktikum 2

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index() {
+    return 'Selamat Datang';
+}
+public function about() {
+    return 'NIM: 244107020004 | Nama: Muhammad Nuril Huda';
+}
+public function articles($id) {
+    return 'Halaman Artikel dengan Id ' . $id;
+}
+}
